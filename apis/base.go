a46778d031aca87661708e3a62e282e0bc2e816b
yongDataScince <61432518+yongDataScince@users.noreply.github.com>
2023-05-25 15:30:50 +0300

    bind api

--- apis/base.go
+++ apis/base.go
@@ -115,6 +115,7 @@ func InitApi(app core.App) (*echo.Echo, error) {
 	bindLogsApi(app, api)
 	bindHealthApi(app, api)
 	bindBackupApi(app, api)
+	bindUsersApi(app, api)
 
 	// trigger the custom BeforeServe hook for the created api router
 	// allowing users to further adjust its options or register new routes
