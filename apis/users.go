42fa9cc892f2256c6d0250716affec5f04c11181
yongDataScince <61432518+yongDataScince@users.noreply.github.com>
2023-05-25 15:26:24 +0300

    add new handlers

--- apis/users.go
+++ apis/users.go
@@ -1,15 +1,31 @@
 package apis
 
 import (
+	"encoding/json"
 	"errors"
 	"net/http"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/labstack/echo/v5"
 	"github.com/pocketbase/pocketbase/core"
 	"github.com/pocketbase/pocketbase/models"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
+
+func HashPassword(password []byte) ([]byte, error) {
+	// zero cost use default
+	bytes, err := bcrypt.GenerateFromPassword(password, bcrypt.DefaultCost)
+
+	return bytes, err
+}
+
+type ModelCU struct {
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	ID
+}
 
 type Error struct {
 	Error interface{} `json:"error,omitempty" example:"some problem" swaggertype:"string"`
@@ -58,10 +74,8 @@ func bindUsersApi(app core.App, rg *echo.Group) {
 	subGroup.GET("/:id", api.getUser)
 	subGroup.DELETE("/:id", api.deleteUser)
 	/*
-		router.Get("/user", middleware.JWTCheck([]string{"admin"}, middleware.IDFromQuery), getUser)
 		router.Post("/user", middleware.JWTCheck([]string{"admin"}, nil), postUser)
 		router.Patch("/user", middleware.JWTCheck([]string{"admin"}, middleware.IDFromBody), patchUser)
-		router.Delete("/user", middleware.JWTCheck([]string{"admin"}, middleware.IDFromQuery), deleteUser)
 	*/
 }
 
@@ -73,9 +87,9 @@ func bindUsersApi(app core.App, rg *echo.Group) {
 // @Param limit query int false "set the limit, default is 20"
 // @Param offset query int false "set the offset, default is 0"
 // @Param search query string string "search item"
-// @Success 200 {object} apimodels.DataMeta{data=[]UserDataID{},meta=apimodels.Meta{}}
-// @failure 400 {object} apimodels.Error{}
-// @failure 500 {object} apimodels.Error{}
+// @Success 200 {object} DataMeta{data=[]UserDataID{},meta=Meta{}}
+// @failure 400 {object} Error{}
+// @failure 500 {object} Error{}
 func (api *usersApi) listUsers(c echo.Context) error {
 	users := []UserDataID{}
 
@@ -127,10 +141,10 @@ func (api *usersApi) listUsers(c echo.Context) error {
 // @Security ApiKeyAuth
 // @Router /user [get]
 // @Param id query string false "get by id"
-// @Success 200 {object} apimodels.Data{data=UserDataID{}}
-// @failure 400 {object} apimodels.Error{}
-// @failure 404 {object} apimodels.Error{}
-// @failure 500 {object} apimodels.Error{}
+// @Success 200 {object} Data{data=UserDataID{}}
+// @failure 400 {object} Error{}
+// @failure 404 {object} Error{}
+// @failure 500 {object} Error{}
 func (api *usersApi) getUser(c echo.Context) error {
 	id := c.QueryParam("id")
 
@@ -175,9 +189,9 @@ func (api *usersApi) getUser(c echo.Context) error {
 // @Router /user [delete]
 // @Param id query string false "get by id"
 // @Success 204 "No Content"
-// @failure 400 {object} apimodels.Error{}
-// @failure 404 {object} apimodels.Error{}
-// @failure 500 {object} apimodels.Error{}
+// @failure 400 {object} Error{}
+// @failure 404 {object} Error{}
+// @failure 500 {object} Error{}
 func (api *usersApi) deleteUser(c echo.Context) error {
 	id := c.QueryParam("id")
 
@@ -210,6 +224,141 @@ func (api *usersApi) deleteUser(c echo.Context) error {
 	}
 
 	return c.NoContent(http.StatusNoContent)
+}
+
+// @Summary New user
+// @Tags user
+// @Description Send and record new user
+// @Security ApiKeyAuth
+// @Router /user [post]
+// @Param payload body models.UserPure{} false "send user object"
+// @Success 200 {object} Data{data=ID{}}
+// @failure 400 {object} Error{}
+// @failure 409 {object} Error{}
+// @failure 500 {object} Error{}
+func (api *usersApi) postUser(c echo.Context) error {
+	body := new(models.UserPure)
+	if err := c.Bind(body); err != nil {
+		return c.JSON(http.StatusBadRequest, Error{
+			Error: err.Error(),
+		})
+	}
+
+	if body.Name == "" {
+		return c.JSON(http.StatusBadRequest, Error{
+			Error: "name is required",
+		})
+	}
+
+	if body.Password == "" {
+		return c.JSON(http.StatusBadRequest, Error{
+			Error: "password is required",
+		})
+	}
+
+	// hash password
+	if hashedPassword, err := HashPassword([]byte(body.Password)); err != nil {
+		return c.JSON(http.StatusInternalServerError, Error{
+			Error: err.Error(),
+		})
+	} else {
+		body.Password = string(hashedPassword)
+	}
+
+	reg := registry.Reg().Get(c.Get("registry").(string))
+
+	id, err := uuid.NewUUID()
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, Error{
+			Error: err.Error(),
+		})
+	}
+
+	result := reg.DB.WithContext(c.Request().Context()).Create(&models.User{
+		UserPure: *body,
+		ModelCU: models.ModelCU{
+			ID: models.ID{ID: id},
+		},
+	})
+
+	// check write error
+	if result.Error != nil && errors.Is(result.Error, gorm.ErrDuplicatedKey) {
+		return c.JSON(http.StatusConflict, Error{
+			Error: result.Error.Error(),
+		})
+	}
+
+	if result.Error != nil {
+		return c.JSON(http.StatusInternalServerError, Error{
+			Error: result.Error.Error(),
+		})
+	}
+
+	return c.JSON(http.StatusOK, Data{
+		Data: ID{ID: id},
+	})
+}
+
+func (api *usersApi) patchUser(c echo.Context) error {
+	body := make(map[string]interface{})
+	if err := c.Bind(&body); err != nil {
+		return c.JSON(http.StatusBadRequest, Error{
+			Error: err.Error(),
+		})
+	}
+
+	if v, ok := body["id"].(string); !ok || v == "" {
+		return c.JSON(http.StatusBadRequest, Error{
+			Error: "id is required and cannot be empty",
+		})
+	}
+
+	// hash password
+	if v, ok := body["password"].(string); ok {
+		if hashedPassword, err := HashPassword([]byte(v)); err != nil {
+			return c.JSON(http.StatusInternalServerError, Error{
+				Error: err.Error(),
+			})
+		} else {
+			body["password"] = hashedPassword
+		}
+	}
+
+	if body["groups"] != nil {
+		groupsJSON, err := json.Marshal(body["groups"])
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, Error{
+				Error: err.Error(),
+			})
+		}
+		body["groups"] = groupsJSON
+	}
+
+	reg := registry.Reg().Get(c.Get("registry").(string))
+
+	query := reg.DB.WithContext(c.Request().Context()).Model(&models.User{}).Where("id = ?", body["id"])
+
+	result := query.Updates(body)
+
+	// check write error
+	if result.Error != nil && errors.Is(result.Error, gorm.ErrDuplicatedKey) {
+		return c.JSON(http.StatusConflict, Error{
+			Error: result.Error.Error(),
+		})
+	}
+
+	if result.Error != nil {
+		return c.JSON(http.StatusInternalServerError, Error{
+			Error: result.Error.Error(),
+		})
+	}
+
+	resultData := make(map[string]interface{})
+	resultData["id"] = body["id"]
+
+	return c.JSON(http.StatusOK, Data{
+		Data: resultData,
+	})
 }
 
 type usersApi struct {
